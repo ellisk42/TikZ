09d905073e52cfb89209a312aa60569a2503bd40
Kevin Ellis <ellisk@mit.edu>
2017-05-23 15:04:35 -0400

    mixtureDensityNetwork.py

---
+++ mixtureDensityNetwork.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plot
+import tensorflow as tf
+
+def mixtureDensityLayer(components, inputs):
+    # predicted means
+    u = tf.layers.dense(inputs, components)
+    # predicted variance
+    v = tf.layers.dense(inputs, components, activation = tf.exp)
+    # mixture coefficients
+    p = tf.layers.dense(inputs, components, activation = tf.nn.softmax)
+
+    return (u,v,p)
+
+def mixtureDensityLogLikelihood((u,v,p), target):
+    print "u = ",u
+    print "target = ",target
+    components = u.shape[1]
+    print "stacked target = ",tf.stack([target]*components,axis = 1)
+    
+    d = u - tf.stack([target]*components,axis = 1)
+    logLikelihoods = -d*d*tf.reciprocal(2.0*v) - 0.5*tf.log(v) + tf.log(p)
+
+    return tf.reduce_logsumexp(logLikelihoods,axis = 1)
+
+def sampleMixture(u,v,p):
+    components = len(u)
+    j = np.random.choice(range(components),p = p)
+    return np.random.normal()*(v[j]**0.5) + u[j]
+
+if __name__ == '__main__':
+    NSAMPLE = 1000
+    
+    regressionInput = tf.placeholder(tf.float32, [NSAMPLE,1])
+    regressionOutput = tf.placeholder(tf.float32, [NSAMPLE])
+
+    hidden = tf.layers.dense(regressionInput, 15,
+                             activation = tf.nn.sigmoid)
+    
+    mixtureOutput = mixtureDensityLayer(5, hidden)
+    mixtureLikelihoods = mixtureDensityLogLikelihood(mixtureOutput, regressionOutput)
+
+    loss =  - tf.reduce_sum(mixtureLikelihoods)
+    optimize = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+
+
+    x_data = np.float32(np.random.uniform(-10.5, 10.5, (1, NSAMPLE))).T
+    r_data = np.float32(np.random.normal(size=(NSAMPLE,1)))
+    y_data = np.float32(np.sin(0.75*x_data)*7.0+x_data*0.5+r_data*1.0)
+
+    (x_data,y_data) = (y_data,x_data)
+
+    print x_data.shape,regressionInput
+    print y_data.shape,regressionOutput
+
+
+    s = tf.Session()
+    s.run(tf.global_variables_initializer())
+
+    for _ in range(10000):
+        print s.run([loss,optimize],
+                    feed_dict = {regressionInput: x_data.reshape((NSAMPLE,1)),
+                                 regressionOutput: y_data.reshape((NSAMPLE,))})[0]
+
+
+    (predictMeans,predictVariance,predictMixture) = s.run(list(mixtureOutput),
+                                                          feed_dict = {regressionInput: x_data.reshape((NSAMPLE,1))})
+    print predictMeans.shape
+    print predictVariance.shape
+    print predictMixture.shape
+    print predictMeans[0]
+    y_predicted = [sampleMixture(predictMeans[j],predictVariance[j],predictMixture[j])
+                   for j in range(NSAMPLE) ]
+
+    
+    plot.figure(figsize=(8, 8))
+    plot_out = plot.plot(x_data,y_data,'ro',alpha=0.3)
+    plot_out = plot.plot(x_data,y_predicted,'bo',alpha=0.3)
+    plot.show()
+
