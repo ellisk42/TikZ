a0db06ff166e69f3dc89d374850bbb4995ea5521
Kevin Ellis <ellisk@mit.edu>
2018-05-05 11:06:48 -0400

    success statistics for no choice baseline

--- noTraceBaseline.py
+++ noTraceBaseline.py
@@ -366,6 +366,29 @@ if __name__ == "__main__":
             
 
         os.exit(0)
+
+    if 'statistics' in sys.argv:
+        from groundTruthParses import *
+        import os
+        results = []
+        gt = []
+        for n in xrange(100):
+            gt.append(getGroundTruthParse('drawings/expert-%s.png'%(n)))
+            f = "noTraceOutputs/%d.p"%n
+            try:
+                with open(f,"rb") as handle:
+                    result = pickle.load(handle)
+                results.append(result)
+            except:
+                results.append(None)
+        times = [ r["time"] if r else float('inf')
+                  for r in results ]
+        medianTime = sorted(times)[len(times)/2]
+        print "Median time",medianTime
+        successful = sum(r is not None and len(r["spec"] - g) == 0
+                         for r,g in zip(results,gt) )
+        print "# times that we got a program which was consistent with the data",successful
+        os.exit(0)
         
     #print "# Learnable parameters:",sum([ parameter.view(-1).shape[0] for parameter in model.parameters() ])
         
